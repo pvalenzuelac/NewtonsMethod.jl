2ecdafcbeddf3e1566fe11e62c3e59a701ca06ee
pvalenzuelac <pdvalenzuela@uc.cl>
2020-10-20 15:25:40 -0700

    adding documentation

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 [![Build Status](https://travis-ci.com/pvalenzuelac/NewtonsMethod.jl.svg?branch=master)](https://travis-ci.com/pvalenzuelac/NewtonsMethod.jl)
 [![Coverage](https://codecov.io/gh/pvalenzuelac/NewtonsMethod.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/pvalenzuelac/NewtonsMethod.jl)
+
+This repository contains two 
--- src/newtonroot.jl
+++ src/newtonroot.jl
@@ -1,4 +1,36 @@
 
+"""
+newtonroot(f[, f']; x0 = 0.8, tolerance = 1.e-13, maxiter = 1000)
+This function find the local root of a univariate smooth function `f(x)` using the Newton's Method. This method starts with    Starting with an $ x_0 $ guess, a function $ f(\cdot) $ and the first-derivative $ f'(\cdot) $, the algorithm is to repeat
+
+    $$
+    x^{n+1} = x^n - \frac{f(x^n)}{f'(x^n)}
+    $$
+...
+# Arguments
+- `f(x)::Function`: the function you are trying to solve.
+- `f'(x)::Function`: the first derivative of the function you are trying to solve. If it is not given the program estimated `f'(x)` via ForwardDiff.
+- `x0::Float64`: the initial value for the algorithm. The default value is 0.8.
+- `tolerance::Float64` : the minimimum tolerance for the algorithm to converge. The default value is 1.0e-13.
+- `maxiter::Int64`: the maximum number of iterations allowed. The default value is 1000.
+---
+# Examples
+```julia
+julia> f(x)  =  (x - 2)^2
+f (generic function with 1 method)
+
+julia> df(x) = 2(x - 2)
+df (generic function with 1 method)
+
+julia> newtonroot(f,df)
+(value = 1.9999999999999318, normdiff = 6.816769371198461e-14, iter = 45)
+
+julia> a = newtonroot(f)[1]
+1.9999999999999318
+```
+...
+
+"""
 function newtonroot(f, f_prime; x0=0.8, tolerance=1.0E-13, maxiter=1000)
     # Initial values
     v_old = x0
@@ -23,8 +55,6 @@ function newtonroot(f, f_prime; x0=0.8, tolerance=1.0E-13, maxiter=1000)
         return(value = nothing, normdiff = nothing, iter = nothing)
     end
 end
-
-
 
 function newtonroot(f; x0=0.8, tolerance=1.0E-13, maxiter=1000)
     # Initial values
